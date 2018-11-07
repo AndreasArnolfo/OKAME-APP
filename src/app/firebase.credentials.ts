8d3e0987aeda50b0a1ded28ab59a2b0cc27ba142
andreas <andreas.arnolfo@gmail.com>
2018-11-07 15:28:22 +0100

    fix: firebase add donées

--- src/app/firebase.credentials.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC-kJOMDCyychOCPhw1nJgu6wawfp0rYbs",
-  authDomain: "okame-26254.firebaseapp.com",
-  databaseURL: "https://okame-26254.firebaseio.com",
-  projectId: "okame-26254",
-  storageBucket: "okame-26254.appspot.com",
-  messagingSenderId: "71426874483"
-};
